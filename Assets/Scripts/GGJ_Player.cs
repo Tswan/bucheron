205775d8814e2df810deec67cd9983a88b96d952
Matt Beddome <MattBeddome@cunet.carleton.ca>
2017-01-20 19:14:47 -0500

    * Added empty script for player. * Added empty script for spawner.

---
+++ Assets/Scripts/GGJ_Player.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GGJ_Player : MonoBehaviour
+{
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+}
