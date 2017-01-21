356e97ea1423291232f2e4782bc5f5f51ca0b90c
= <=>
2017-01-20 19:55:13 -0500

    * Changed player health to be publicly settable by the editor. * Changed the player to shake the camera on damage.

--- Assets/Scripts/GGJ_Player.cs
+++ Assets/Scripts/GGJ_Player.cs
@@ -1,13 +1,24 @@
-﻿using UnityEngine;
+﻿using System;
 using System.Collections;
+
+using UnityEngine;
 
 public class GGJ_Player : MonoBehaviour
 {
-    private int _hitPoints;
+    public int HitPoints { get; set; }
+    public Camera MainCamera { get; set; }
+
+    private int _currency;
+    private DateTime _startTime;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
 
     private void Start()
     {
-
+        _startTime = DateTime.UtcNow;
     }
 
     private void Update()
@@ -22,10 +33,13 @@ public class GGJ_Player : MonoBehaviour
         // TODO: Shake camera
 
         // Decrement health
-        _hitPoints -= damage;
-        if (_hitPoints >= 0)
+        HitPoints -= damage;
+        if (HitPoints >= 0)
         {
             // TODO: Kill player
         }
+
+        // Shake the camera for an amount of time dependant on the damage
+        MainCamera.GetComponent<CameraShake>().ShakeTime = damage * 0.1f;
     }
 }
