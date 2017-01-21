02a4229428e89b3e168ff9a3df5650053606f01b
Sam Albon <sam.m.albon@outlook.com>
2017-01-20 23:00:39 -0500

    * Changed camera to lock on the x axis, this works with the camera shake script.

---
+++ Assets/Scripts/Cameras/GGJ_CameraShake.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class GGJ_CameraShake : GGJ_Camera
+{
+    // How long the object should shake for.
+    [HideInInspector]
+	public float ShakeTime;
+
+    // Amplitude of the shake. A larger value shakes the camera harder.
+	public float ShakeIntensity;
+	public float DecreaseFactor;
+
+    protected override Vector3 CalculatePosition()
+    {
+        // Calculate the base position
+        var position = base.CalculatePosition();
+
+        // Apply camera shake
+        if (ShakeTime > 0.0f)
+        {
+            position += Random.insideUnitSphere * ShakeIntensity;
+            ShakeTime -= Time.deltaTime * DecreaseFactor;
+        }
+        else
+        {
+            ShakeTime = 0.0f;
+        }
+
+        // Return the position
+        return position;
+    }
+}
