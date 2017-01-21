800f177d516cfdac4295d537fcde9c7b45359df5
= <=>
2017-01-20 19:46:43 -0500

    * Changed GGJ_Spawner so it begins spawning a set number of enemies using some basic sine wave calculation that probably won't work. Currently nothing spawns, however it counts up until it reaches it max amount.

--- Assets/Scripts/Spawners/GGJ_Spawner.cs
+++ Assets/Scripts/Spawners/GGJ_Spawner.cs
@@ -3,6 +3,12 @@ using System.Collections;
 
 public class Spawner : MonoBehaviour
 {
+    private GGJ_Player _collidedPlayer;
+    private float _timeElapsedSinceLastSpawn;
+    private int _spawnedCount;
+
+    public int MaxSpawnCount { get; set; }
+
     void Awake()
     {
         DontDestroyOnLoad(this);
@@ -10,10 +16,44 @@ public class Spawner : MonoBehaviour
 
     void Start()
     {
-
+        _collidedPlayer = null;
+        _timeElapsedSinceLastSpawn = 0.0f;
+        _spawnedCount = 0;
     }
     
     void Update()
     {
+        // Check whether we're spawning yet
+        if (_collidedPlayer != null)
+        {
+            // Check whether we should spawn an enemy
+            if (Mathf.Sin(_timeElapsedSinceLastSpawn) > 0.0f)
+            {
+                // TODO: Spawn enemy
+
+                // Increment the spawned count
+                _spawnedCount++;
+
+                // Reset spawn timer
+                _timeElapsedSinceLastSpawn = 0.0f;
+
+                // Check whether we should still be running after this
+                if (_spawnedCount >= MaxSpawnCount)
+                {
+                    // Destory this as we're done with it
+                    Destroy(this);
+                }
+            }
+            else
+            {
+                // Increment the time
+                _timeElapsedSinceLastSpawn += Time.deltaTime;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        _collidedPlayer = other.gameObject.GetComponent<GGJ_Player>();
     }
 }
