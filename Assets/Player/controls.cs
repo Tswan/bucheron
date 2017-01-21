620d4ac0362e28ebb2ce20b56f459074c9002dfc
beddome42 <beddome42@gmail.com>
2017-01-20 19:22:58 -0500

    Controller

--- Assets/Player/controls.cs
+++ Assets/Player/controls.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class controls : MonoBehaviour {
 
 	public float moveSpeed;
+	public float jumpSpeed;
+	public bool grounded;
+
 
 	private float x;
 	private float y;
@@ -11,13 +14,60 @@ public class controls : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		grounded = true;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 
 		checkMovement ();
+
+		if (Input.GetAxis ("Y") > 0) {
+			meleeAttack ();
+		}
+		if (Input.GetAxis ("B") > 0) {
+			if (grounded) 
+			{
+				
+			}
+			if (gameObject.GetComponent<Rigidbody> ().velocity.y <= 0.01f && gameObject.GetComponent<Rigidbody> ().velocity.y >= -0.01f) {
+				jump ();
+			}
+		}
+		if (Input.GetAxis ("A") > 0) {
+			print ("A");
+		}
+		if (Input.GetAxis ("X") > 0) {
+			rangedAttack ();
+		}
+		if (Input.GetAxis ("L") > 0) {
+			
+		}
+		if (Input.GetAxis ("R") > 0) {
+			
+		}
+		if (Input.GetAxis ("Start") > 0) {
+			
+		}
+		if (Input.GetAxis ("Select") > 0) {
+			
+		}
+
+	}
+
+	private void jump()
+	{
+		grounded = false;
+		gameObject.GetComponent<Rigidbody> ().AddForce (transform.up * jumpSpeed);
+	}
+
+	private void meleeAttack()
+	{
+
+	}
+
+	private void rangedAttack()
+	{
 
 	}
 
---
+++ Assets/Player/stats.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class stats : MonoBehaviour {
+
+	public int healthMax;
+	public int healthCurr;
+	public int attack;
+	public int defense;
+
+	public int ammo;
+
+	// Use this for initialization
+	void Start () {
+		healthCurr = healthMax;
+	}
+
+}
