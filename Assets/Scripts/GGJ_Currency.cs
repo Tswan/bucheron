9cad2ef86f1b360bcc0a00a6720c6dae3b30d70b
Matt Beddome <MattBeddome@cunet.carleton.ca>
2017-01-20 21:33:48 -0500

    * Added currency script object for physical currency object in game.

---
+++ Assets/Scripts/GGJ_Currency.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class GGJ_Currency : MonoBehaviour
+    {
+        public int CurrencyAmount;
+
+        private void Awake()
+        {
+            DontDestroyOnLoad(this);
+        }
+
+        private void Start()
+        {
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            // Check whether the other object is a player
+            var player = other.gameObject.GetComponent<GGJ_Player>();
+            if (player != null)
+            {
+                // Add the currency amount to the player stats object
+                player.Currency += CurrencyAmount;
+
+                // DEBUG: Log that the player is ignoring this object
+                Debug.Log(string.Format("Adding {0} to player ({1}), player now has {2}.", CurrencyAmount, player.tag, player.Currency));
+
+                // Destory this object
+                Destroy(gameObject);
+            }
+        }
+    }
+}
