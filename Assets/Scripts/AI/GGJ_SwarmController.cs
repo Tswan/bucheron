c29f45d8ea07da8a272c7bd831139d48aa5565a1
Sam Albon <sam.m.albon@outlook.com>
2017-01-21 11:24:31 -0500

    * Added missing files.

---
+++ Assets/Scripts/AI/GGJ_SwarmController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class GGJ_SwarmController : MonoBehaviour
+{
+    [HideInInspector]
+    public List<GGJ_EnemyController> Enemies { get; private set; }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    private void Start()
+    {
+        Enemies = new List<GGJ_EnemyController>();
+    }
+
+    private void Update()
+    {
+    }
+}
