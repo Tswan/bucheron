9bbb7871810c5a6917c99a66cad374721a7bc1ea
Sam Albon <sam.m.albon@outlook.com>
2017-01-22 12:41:35 -0500

    * Added snowball attack for snowman.

--- Assets/Scripts/GGJ_Snowman.cs
+++ Assets/Scripts/GGJ_Snowman.cs
@@ -4,23 +4,32 @@ using System.Collections;
 public class GGJ_Snowman : GGJ_EnemyController {
 
     public GameObject snowball;
-
+    public GameObject hand;
+    private GameObject player;
 	// Use this for initialization
 	protected override void Start () {
         base.Start();
         throwSnowball();
-
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     protected override void FixedUpdate()
     { 
         base.FixedUpdate();
-	}
+
+        Vector2 angle = Vector3.AngleBetween(new Vector3(gameObject.transform.position.x, 0, gameObject.transform.position.z), new Vector3(player.transform.position.x, 0, player.transform.position.z));
+
+    }
 
     private void throwSnowball()
     {
-        GameObject newSnowball = Instantiate(snowball, gameObject.transform.position, Quaternion.identity) as GameObject;
+        myAnim.Play("throwSnowball");
+    }
+
+    private void makeSnowball()
+    {
+        GameObject newSnowball = Instantiate(snowball, hand.transform.position, Quaternion.identity) as GameObject;
     }
 
 }
--- Assets/Scripts/Snowball.cs
+++ Assets/Scripts/Snowball.cs
@@ -1,15 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Snowball : MonoBehaviour {
+public class Snowball : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private Vector3 target;
+    Rigidbody rb;
+    Vector3 direction;
+    // Use this for initialization
+    void Start()
+    {
+        Vector3 target = GameObject.FindGameObjectWithTag("Player").transform.position;
+        print(target);
+        rb = GetComponent<Rigidbody>();
+        //rb.AddForce(Vector3.Normalize(target - transform.position) * 50, ForceMode.Force);
+        rb.velocity = Vector3.Normalize(target - transform.position) * 30;
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        var playerController = col.gameObject.GetComponent<GGJ_PlayerController>();
+        if (playerController != null)
+        {
+            playerController.Stats.TakeDamage(gameObject, 5);
+        }
+
+        Destroy(gameObject);
+
+    }
+
+    // Update is called once per frame
 }
